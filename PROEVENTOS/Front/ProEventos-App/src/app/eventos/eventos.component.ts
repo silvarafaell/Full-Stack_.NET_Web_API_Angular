1dacc1ad2c36091372f18c00cd07e5572d188ed9
Francisco Rafael <francisco_rafael@hotmail.com.br>
2022-06-06 16:25:00 -0300

    Propriedade Binding parte 02

--- PROEVENTOS/Front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ PROEVENTOS/Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -9,13 +9,18 @@ import { Component, OnInit } from '@angular/core';
 export class EventosComponent implements OnInit {
 
   public eventos: any = [];
-  widthImg: number = 50;
-  marginImg: number = 2;
+  larguraImagem: number = 150;
+  margemImagem: number = 2;
+  exibirImagem: boolean = true;
 
   constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
     this.getEventos();
+  }
+
+  alterarImagem() {
+    this.exibirImagem = !this.exibirImagem;
   }
 
   public getEventos(): void {
