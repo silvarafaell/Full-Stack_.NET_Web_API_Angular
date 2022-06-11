cd57953f24a1be931f2cf9ce02d3383055ea11bc
Francisco Rafael <francisco_rafael@hotmail.com.br>
2022-06-11 07:15:12 -0300

    Interface Persistencia

---
+++ PROEVENTOS/Back/src/ProEventos.Persistence/IProEventosPersistence.cs
@@ -0,0 +1,26 @@
+using System.Threading.Tasks;
+using ProEventos.Domain;
+
+namespace ProEventos.Persistence
+{
+    public interface IProEventosPersistence
+    {
+        //Geral
+        void Add<T>(T entity) where T : class;
+        void Update<T>(T entity) where T : class;
+        void Delete<T>(T entity) where T : class;
+        void DeleteRange<T>(T[] entity) where T : class;
+
+        Task<bool> SaveChangesAsync();
+
+        //Eventos
+        Task<Evento[]> GetAllEventosByTemaAsync(string tema, bool includePalestrantes);
+        Task<Evento[]> GetAllEventosAsync(bool includePalestrantes);
+        Task<Evento> GetAllEventoByIdAsync(int EventoId, bool includePalestrantes);
+
+        //Palestrantes 
+        Task<Palestrante[]> GetAllPalestrantesByNomeAsync(string Nome, bool includeEventos);
+        Task<Palestrante[]> GetAllPalestrantesAsync(bool includeEventos);
+        Task<Palestrante> GetAllPalestranteByIdAsync(int PalestranteId, bool includeEventos);
+    }
+}
