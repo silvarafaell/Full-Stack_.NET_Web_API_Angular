a1822bda21cc12952154cc2b33e44931f2e578d0
Francisco Rafael <francisco_rafael@hotmail.com.br>
2022-06-12 23:10:16 -0300

    Newtonsoft Loop Infinito

--- PROEVENTOS/Back/src/ProEventos.API/Startup.cs
+++ PROEVENTOS/Back/src/ProEventos.API/Startup.cs
@@ -28,7 +28,10 @@ namespace ProEventos.API
             services.AddDbContext<ProEventosContext>(
                 context => context.UseSqlite(Configuration.GetConnectionString("Default"))
             );
-            services.AddControllers();
+            services.AddControllers()
+                    .AddNewtonsoftJson(x => x.SerializerSettings.ReferenceLoopHandling =
+                        Newtonsoft.Json.ReferenceLoopHandling.Ignore
+                    );
 
             services.AddScoped<IEventoService, EventoService>();
             services.AddScoped<IGeralPersist, GeralPersist>();
