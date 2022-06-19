06e12b616e8fe628f1a7481d3ace41bd52daee42
Francisco Rafael <francisco_rafael@hotmail.com.br>
2022-06-19 16:54:21 -0300

    Interface Model - Parte 02

--- PROEVENTOS/Front/ProEventos-App/src/app/models/Evento.ts
+++ PROEVENTOS/Front/ProEventos-App/src/app/models/Evento.ts
@@ -1,13 +1,17 @@
+import { Lote } from "./Lote";
+import { Palestrante } from "./Palestrante";
+import { RedeSocial } from "./RedeSocial";
+
 export interface Evento {
-  Id: number;
-  Local: string;
-  DataEvento?: Date;
-  Tema: string;
-  QtdPessoas: number;
-  ImagemURL: string;
-  Telefone: string;
-  Email: string;
-  Lotes: Lote[];
-  RedesSociais: RedeSocial[];
-  PalestrantesEventos: Palestrante[];
+  id: number;
+  local: string;
+  dataEvento?: Date;
+  tema: string;
+  qtdPessoas: number;
+  imagemURL: string;
+  telefone: string;
+  email: string;
+  lotes: Lote[];
+  redesSociais: RedeSocial[];
+  palestrantesEventos: Palestrante[];
 }
