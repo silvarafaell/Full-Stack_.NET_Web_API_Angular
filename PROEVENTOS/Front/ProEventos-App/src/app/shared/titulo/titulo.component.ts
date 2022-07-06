41c5cdf98fe4d131a952dcbedb21b6bdb7d307b0
Francisco Rafael <francisco_rafael@hotmail.com.br>
2022-07-05 23:58:57 -0300

    Component Titulo

--- PROEVENTOS/Front/ProEventos-App/src/app/shared/titulo/titulo.component.ts
+++ PROEVENTOS/Front/ProEventos-App/src/app/shared/titulo/titulo.component.ts
@@ -7,6 +7,9 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class TituloComponent implements OnInit {
   @Input() titulo = '';
+  @Input() iconClass = 'fa fa-user';
+  @Input() subtitulo = 'Desde 2021';
+  @Input() botaoListar = false;
   constructor() { }
 
   ngOnInit(): void {
