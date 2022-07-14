4f688f85380d29cc81409ad4013759a78875515f
Francisco Rafael <francisco_rafael@hotmail.com.br>
2022-07-14 07:45:40 -0300

    Rotas do User

--- PROEVENTOS/Front/ProEventos-App/src/app/app.module.ts
+++ PROEVENTOS/Front/ProEventos-App/src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { TituloComponent } from './shared/titulo/titulo.component';
-import { PerfilComponent } from './components/perfil/perfil.component';
+import { PerfilComponent } from './components/user/perfil/perfil.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { ContatosComponent } from './components/contatos/contatos.component';
 import { EventoService } from './services/Evento.service';
