0871e308848c678e654e032628a0693444388659
Francisco Rafael <francisco_rafael@hotmail.com.br>
2022-07-17 14:22:12 -0300

    Form Builder e Limpando o codigo

--- PROEVENTOS/Front/ProEventos-App/src/app/components/eventos/evento-detalhe/evento-detalhe.component.ts
+++ PROEVENTOS/Front/ProEventos-App/src/app/components/eventos/evento-detalhe/evento-detalhe.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-evento-detalhe',
@@ -9,25 +9,26 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 export class EventoDetalheComponent implements OnInit {
 
   form!: FormGroup;
-  constructor() { }
+
+  get f(): any {
+    return this.form.controls;
+  }
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
     this.validation();
   }
 
   public validation(): void {
-    this.form = new FormGroup({
-      local: new FormControl('', Validators.required),
-      dataEvento: new FormControl('', Validators.required),
-      tema: new FormControl('',
-        [Validators.required, Validators.minLength(4), Validators.maxLength(50)]),
-      qtdPessoas: new FormControl('',
-        [Validators.required, Validators.max(120000)]),
-      imagemURL: new FormControl('', Validators.required),
-      telefone: new FormControl('', Validators.required),
-      email: new FormControl('',
-        [Validators.required, Validators.email]),
+    this.form = this.fb.group({
+      local: ['', Validators.required],
+      dataEvento: ['', Validators.required],
+      tema: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(50)]],
+      qtdPessoas: ['', [Validators.required, Validators.max(120000)]],
+      imagemURL: ['', Validators.required],
+      telefone: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
     });
   }
-
 }
