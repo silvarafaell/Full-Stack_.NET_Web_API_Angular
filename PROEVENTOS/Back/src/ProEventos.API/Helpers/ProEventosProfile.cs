dff779c25d06a506e3cc711e3e5d3524792b7e66
Francisco Rafael <francisco_rafael@hotmail.com.br>
2022-07-21 07:39:48 -0300

    AutoMapper e Profile

---
+++ PROEVENTOS/Back/src/ProEventos.API/Helpers/ProEventosProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using ProEventos.API.Dtos;
+using ProEventos.Domain;
+
+namespace ProEventos.API.Helpers
+{
+    public class ProEventosProfile : Profile
+    {
+        public ProEventosProfile()
+        {
+            CreateMap<Evento, EventoDto>();
+        }
+    }
+}
