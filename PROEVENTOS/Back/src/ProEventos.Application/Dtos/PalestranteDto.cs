8beea0442a265f9f40bf272f3b3fecced87ccc15
Francisco Rafael <francisco_rafael@hotmail.com.br>
2022-07-26 20:26:46 -0300

    Classes Dtos Restantes

--- PROEVENTOS/Back/src/ProEventos.Application/Dtos/PalestranteDto.cs
+++ PROEVENTOS/Back/src/ProEventos.Application/Dtos/PalestranteDto.cs
@@ -2,17 +2,14 @@
 
 namespace ProEventos.Application.Dtos
 {
-    public class EventoDto
+    public class PalestranteDto
     {
         public int Id { get; set; }
-        public string Local { get; set; }
-        public string DataEvento { get; set; }
-        public string Tema { get; set; }
-        public int QtdPessoas { get; set; }
+        public string Nome { get; set; }
+        public string MiniCurriculo { get; set; }
         public string ImagemURL { get; set; }
         public string Telefone { get; set; }
         public string Email { get; set; }
-        public IEnumerable<LoteDto> Lotes { get; set; }
         public IEnumerable<RedeSocialDto> RedesSociais { get; set; }
         public IEnumerable<PalestranteDto> Palestrantes { get; set; }
     }
