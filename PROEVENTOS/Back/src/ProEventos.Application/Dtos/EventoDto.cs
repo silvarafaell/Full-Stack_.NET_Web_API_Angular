e46be3d8ce4e02154e9057e991129f289d5fc448
Francisco Rafael <francisco_rafael@hotmail.com.br>
2022-07-27 06:21:02 -0300

    Data Annotations

--- PROEVENTOS/Back/src/ProEventos.Application/Dtos/EventoDto.cs
+++ PROEVENTOS/Back/src/ProEventos.Application/Dtos/EventoDto.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace ProEventos.Application.Dtos
 {
@@ -7,10 +8,14 @@ namespace ProEventos.Application.Dtos
         public int Id { get; set; }
         public string Local { get; set; }
         public string DataEvento { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatorio.")]
         public string Tema { get; set; }
         public int QtdPessoas { get; set; }
         public string ImagemURL { get; set; }
         public string Telefone { get; set; }
+
+        [EmailAddress(ErrorMessage = "O campo {0} precisa ser um e-mail válido.")]
         public string Email { get; set; }
         public IEnumerable<LoteDto> Lotes { get; set; }
         public IEnumerable<RedeSocialDto> RedesSociais { get; set; }
