71bfeebd61a56d88ba189fddb7c80973d089092c
Francisco Rafael <francisco_rafael@hotmail.com.br>
2022-07-27 07:41:42 -0300

    Campo com Regex - Validacao Imagem

--- PROEVENTOS/Back/src/ProEventos.Application/Dtos/EventoDto.cs
+++ PROEVENTOS/Back/src/ProEventos.Application/Dtos/EventoDto.cs
@@ -17,6 +17,9 @@ namespace ProEventos.Application.Dtos
         [Display(Name = "Qtd Pessoas")]
         [Range(1, 120000, ErrorMessage = "{0} não poder ser menor que 1 e maior que 120.000")]
         public int QtdPessoas { get; set; }
+
+        [RegularExpression(@".*\.(gif|jpe?g|bmp|png)$",
+                           ErrorMessage = "Não é uma imagem válida. (gif, jpg, jpeg, bmp ou png)")]
         public string ImagemURL { get; set; }
 
         [Required(ErrorMessage = "O campo {0} é obrigatório.")]
