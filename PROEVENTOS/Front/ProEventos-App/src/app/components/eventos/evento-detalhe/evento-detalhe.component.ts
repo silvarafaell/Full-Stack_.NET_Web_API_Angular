a1f379ac2005e21d757bdcb4cfd85c9d890bf159
Francisco Rafael <francisco_rafael@hotmail.com.br>
2022-07-30 08:59:05 -0300

    Carregando Evento

--- PROEVENTOS/Front/ProEventos-App/src/app/components/eventos/evento-detalhe/evento-detalhe.component.ts
+++ PROEVENTOS/Front/ProEventos-App/src/app/components/eventos/evento-detalhe/evento-detalhe.component.ts
@@ -1,6 +1,9 @@
+import { EventoService } from 'src/app/services/Evento.service';
+import { ActivatedRoute } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { BsLocaleService } from 'ngx-bootstrap';
+import { Evento } from '@app/models/Evento';
 
 @Component({
   selector: 'app-evento-detalhe',
@@ -9,6 +12,7 @@ import { BsLocaleService } from 'ngx-bootstrap';
 })
 export class EventoDetalheComponent implements OnInit {
 
+  evento = {} as Evento;
   form!: FormGroup;
 
   get f(): any {
@@ -24,11 +28,32 @@ export class EventoDetalheComponent implements OnInit {
     };
   }
 
-  constructor(private fb: FormBuilder, private localeService: BsLocaleService) {
+  constructor(private fb: FormBuilder,
+    private localeService: BsLocaleService,
+    private router: ActivatedRoute,
+    private eventoService: EventoService) {
     this.localeService.use('pt-br');
   }
 
+  public carregarEvento(): void {
+    const eventoIdParam = this.router.snapshot.paramMap.get('id');
+
+    if (eventoIdParam !== null) {
+      this.eventoService.getEventosById(+eventoIdParam).subscribe(
+        (evento: Evento) => {
+          this.evento = { ...evento };
+          this.form.patchValue(this.evento);
+        },
+        (error: any) => {
+          console.error(error);
+        },
+        () => { },
+      );
+    }
+  }
+
   ngOnInit(): void {
+    this.carregarEvento();
     this.validation();
   }
 
