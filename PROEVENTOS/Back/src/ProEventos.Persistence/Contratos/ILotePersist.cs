2d449dd3128abe605acada46a3b0a21b55f53405
Francisco Rafael <francisco_rafael@hotmail.com.br>
2022-08-04 08:22:49 -0300

    Interface Lote Persistencia

---
+++ PROEVENTOS/Back/src/ProEventos.Persistence/Contratos/ILotePersist.cs
@@ -0,0 +1,11 @@
+using System.Threading.Tasks;
+using ProEventos.Domain;
+
+namespace ProEventos.Persistence.Contratos
+{
+    public interface ILotePersist
+    {
+        Task<Lote[]> GetLotesByEventoIdAsync(int eventoId);
+        Task<Lote> GetLoteByIdAsync(int eventoId, int id);
+    }
+}
