43802a2a087545475a044239f7f9dddcdfaf7430
Francisco Rafael <francisco_rafael@hotmail.com.br>
2022-08-23 07:42:51 -0300

    UserRoles no Contexto

--- PROEVENTOS/Back/src/ProEventos.Persistence/Contextos/ProEventosContext.cs
+++ PROEVENTOS/Back/src/ProEventos.Persistence/Contextos/ProEventosContext.cs
@@ -18,6 +18,24 @@ namespace ProEventos.Persistence.Contextos
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<UserRole>(userRole =>
+                {
+                    userRole.HasKey(ur => new { ur.UserId, ur.RoleId });
+
+                    userRole.HasOne(ur => ur.Role)
+                            .WithMany(r => r.UserRoles)
+                            .HasForeignKey(ur => ur.RoleId)
+                            .IsRequired();
+
+                    userRole.HasOne(ur => ur.User)
+                           .WithMany(r => r.UserRoles)
+                           .HasForeignKey(ur => ur.UserId)
+                           .IsRequired();
+                }
+            );
+
             modelBuilder.Entity<PalestranteEvento>()
                 .HasKey(PE => new { PE.EventoId, PE.PalestranteId });
 
