7f174b90ec6eab5f10e4ad2ae545b6c4495ce528
Francisco Rafael <francisco_rafael@hotmail.com.br>
2022-09-14 07:46:39 -0300

    Tela de Login

--- PROEVENTOS/Front/ProEventos-App/src/app/components/user/login/login.component.ts
+++ PROEVENTOS/Front/ProEventos-App/src/app/components/user/login/login.component.ts
@@ -1,4 +1,8 @@
+import { ToastrService } from 'ngx-toastr';
+import { AccountService } from './../../../services/account.service';
 import { Component, OnInit } from '@angular/core';
+import { UserLogin } from '@app/models/identity/UserLogin';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +10,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  model = {} as UserLogin;
 
-  constructor() { }
+  constructor(private accountService: AccountService, private router: Router, private toaster: ToastrService) { }
 
-  ngOnInit(): void {
+  ngOnInit(): void { }
+
+  public login(): void {
+    this.accountService.login(this.model).subscribe(
+      () => { this.router.navigateByUrl('/dashboard'); },
+      (error: any) => {
+        if (error.status == 401)
+          this.toaster.error('usuário ou senha inválido');
+        else
+          console.error(error);
+      }
+    )
   }
 
 }
