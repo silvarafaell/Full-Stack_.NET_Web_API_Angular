702de3e8a1e480fd46aa76f2c274729e8519bf32
Francisco Rafael <francisco_rafael@hotmail.com.br>
2022-10-11 07:56:42 -0300

    RedeSocial - Save by Evento

---
+++ PROEVENTOS/Back/src/ProEventos.Application/RedeSocialService.cs
@@ -0,0 +1,125 @@
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using ProEventos.Application.Contratos;
+using ProEventos.Application.Dtos;
+using ProEventos.Domain;
+using ProEventos.Persistence.Contratos;
+
+namespace ProEventos.Application
+{
+    public class RedeSocialService : IRedeSocialService
+    {
+        private readonly IRedeSocialPersist _redeSocialPersist;
+        private readonly IMapper _mapper;
+        public RedeSocialService(IRedeSocialPersist redeSocialPersist,
+                                  IMapper mapper)
+        {
+            _redeSocialPersist = redeSocialPersist;
+            _mapper = mapper; 
+        }
+        public async Task AddRedeSocial(int eventoId, RedeSocialDto model)
+        {
+            try
+            {
+                var redeSocial = _mapper.Map<RedeSocial>(model);
+                redeSocial.EventoId = eventoId;
+
+                _redeSocialPersist.Add<RedeSocial>(redeSocial);
+
+                await _redeSocialPersist.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public async Task<RedeSocialDto[]> SaveByEvento(int eventoId, RedeSocialDto[] models)
+        {
+            try
+            {
+                var redeSocials = await _redeSocialPersist.GetAllByEventoIdAsync(eventoId);
+                if (redeSocials == null) return null;
+
+                foreach(var model in models)
+                {
+                    if(model.Id == 0)
+                    {
+                        await AddRedeSocial(eventoId, model);
+                    }
+                    else
+                    {
+                        var redeSocial = redeSocials.FirstOrDefault(redeSocial => redeSocial.Id == model.Id);
+                        model.EventoId = eventoId;
+
+                        _mapper.Map(model, redeSocial);
+
+                        _redeSocialPersist.Update<RedeSocial>(redeSocial);
+
+                        await _redeSocialPersist.SaveChangesAsync();
+                    }
+                }    
+
+                    var redeSocialRetorno = await _redeSocialPersist.GetAllByEventoIdAsync(eventoId);
+
+                    return _mapper.Map<RedeSocialDto[]>(redeSocialRetorno);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public async Task<bool> DeleteLote(int eventoId, int loteId)
+        {
+            try
+            {
+                var lote = await _lotePersist.GetLoteByIdsAsync(eventoId, loteId);
+                if (lote == null) throw new Exception("Lote para delete não encontrado. ");
+
+                _geralPersist.Delete<Lote>(lote);
+                return await _geralPersist.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public async Task<LoteDto> GetLoteByIdsAsync(int eventoId, int loteId)
+        {
+           try
+            {
+                var lote = await _lotePersist.GetLoteByIdsAsync(eventoId, loteId);
+                if (lote == null) return null;
+
+                var resultado = _mapper.Map<LoteDto>(lote);
+
+                return resultado;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public async Task<LoteDto[]> GetLotesByEventoIdAsync(int eventoId)
+        {
+            try
+            {
+                var lotes = await _lotePersist.GetLotesByEventoIdAsync(eventoId);
+                if (lotes == null) return null;
+
+                var resultado = _mapper.Map<LoteDto[]>(lotes);
+
+                return resultado;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+    }
+}
