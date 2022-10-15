85ebafb4160660326d139e31e36f1f47d6bb0897
Francisco Rafael <francisco_rafael@hotmail.com.br>
2022-10-14 23:57:42 -0300

    Testando Palestrante

--- PROEVENTOS/Back/src/ProEventos.API/Startup.cs
+++ PROEVENTOS/Back/src/ProEventos.API/Startup.cs
@@ -82,12 +82,17 @@ namespace ProEventos.API
             services.AddScoped<ILoteService, LoteService>();
             services.AddScoped<ITokenService, TokenService>();
             services.AddScoped<IAccountService, AccountService>();
+            services.AddScoped<IPalestranteService, PalestranteService>();
+            services.AddScoped<IRedeSocialService, RedeSocialService>();
+
 
             services.AddScoped<IGeralPersist, GeralPersist>();
             services.AddScoped<IEventoPersist, EventoPersist>();
             services.AddScoped<ILotePersist, LotePersist>();
 
             services.AddScoped<IUserPersist, UserPersist>();
+            services.AddScoped<IPalestrantePersist, PalestrantePersist>();
+            services.AddScoped<IRedeSocialPersist, RedeSocialPersist>();
 
             services.AddCors();
             services.AddSwaggerGen(options =>
