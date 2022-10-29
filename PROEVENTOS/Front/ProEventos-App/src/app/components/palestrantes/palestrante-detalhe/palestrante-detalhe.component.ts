6162dc3a4f8817eac8ec98411401bf0828597084
Francisco Rafael <francisco_rafael@hotmail.com.br>
2022-10-29 20:12:58 -0300

    Palestrante Detalhe - Parte 01

--- PROEVENTOS/Front/ProEventos-App/src/app/components/palestrantes/palestrante-detalhe/palestrante-detalhe.component.ts
+++ PROEVENTOS/Front/ProEventos-App/src/app/components/palestrantes/palestrante-detalhe/palestrante-detalhe.component.ts
@@ -1,4 +1,8 @@
+import { NgxSpinnerService } from 'ngx-spinner';
+import { FormGroup, FormBuilder } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
+import { PalestranteService } from '@app/services/palestrante.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-palestrante-detalhe',
@@ -6,10 +10,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./palestrante-detalhe.component.scss']
 })
 export class PalestranteDetalheComponent implements OnInit {
+  public form!: FormGroup;
+  public situacaoDoForm = '';
+  public corDaDescricao = '';
 
-  constructor() { }
+  constructor(
+    private fb: FormBuilder,
+    public palestranteService: PalestranteService,
+    private toastr: ToastrService,
+    private spinner: NgxSpinnerService
+  ) { }
 
   ngOnInit() {
+    this.validation();
+  }
+
+  private validation(): void {
+    this.form = this.fb.group({
+      miniCurriculo: ['']
+    })
+  }
+
+  public get f(): any {
+    return this.form.controls;
   }
 
 }
